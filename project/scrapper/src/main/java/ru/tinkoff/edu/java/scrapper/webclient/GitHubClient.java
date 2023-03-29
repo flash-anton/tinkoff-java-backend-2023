04599051afa3e6a8ec4b94bc23e223be83df5b8c
Simonov Anton <flash_anton@mail.ru>
2023-03-29 17:16:04 +0300

    3.2 Практика. url клиентов, версии и прочее вынесены в application.yaml

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/webclient/GitHubClient.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/webclient/GitHubClient.java
@@ -1,31 +1,18 @@
 package ru.tinkoff.edu.java.scrapper.webclient;
 
+import lombok.NonNull;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.tinkoff.edu.java.scrapper.dto.GitHubRepositoryInfoResponse;
 
-public class GitHubClient
+public record GitHubClient( @NonNull WebClient webClient )
 {
-	protected final WebClient client;
-
-	public GitHubClient()
-	{
-		this( null );
-	}
-
-	public GitHubClient( String baseUrl )
-	{
-		client = WebClientBuilder.create( baseUrl, "https://api.github.com" )
-								 .defaultHeader( "Accept", "application/vnd.github+json" )
-								 .defaultHeader( "X-GitHub-Api-Version", "2022-11-28" )
-								 .build();
-	}
-
 	public GitHubRepositoryInfoResponse fetchRepositoryInfo( String user, String repository )
 	{
-		return client.get()
-					 .uri( "/repos/{user}/{repo}", user, repository )
-					 .retrieve()
-					 .bodyToMono( GitHubRepositoryInfoResponse.class )
-					 .block();
+		return webClient
+			.get()
+			.uri( "/repos/{user}/{repo}", user, repository )
+			.retrieve()
+			.bodyToMono( GitHubRepositoryInfoResponse.class )
+			.block();
 	}
 }
