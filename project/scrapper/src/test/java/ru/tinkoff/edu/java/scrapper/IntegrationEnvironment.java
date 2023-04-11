5b11a591fca90dac8eaaf53ea1dad04c78197663
Simonov Anton <flash_anton@mail.ru>
2023-04-11 23:49:47 +0300

    5.4: Добавлен тест проверки запуска контейнера postgres и миграции

---
+++ project/scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationEnvironment.java
@@ -0,0 +1,49 @@
+package ru.tinkoff.edu.java.scrapper;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+abstract public class IntegrationEnvironment
+{
+    static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
+
+    static
+    {
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>( "postgres:15" );
+        POSTGRESQL_CONTAINER.start();
+
+        runMigration();
+    }
+
+    static void runMigration()
+    {
+        String url = POSTGRESQL_CONTAINER.getJdbcUrl();
+        String user = POSTGRESQL_CONTAINER.getUsername();
+        String pass = POSTGRESQL_CONTAINER.getPassword();
+
+        Path path = Path.of(".").toAbsolutePath().getParent().resolve("migrations/");
+
+        try( Connection connection = DriverManager.getConnection( url, user, pass );
+             DirectoryResourceAccessor directoryResourceAccessor = new DirectoryResourceAccessor( path ) )
+        {
+            JdbcConnection jdbcConnection = new JdbcConnection( connection );
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation( jdbcConnection );
+            Liquibase liquibase = new liquibase.Liquibase( "master.xml", directoryResourceAccessor, database );
+            liquibase.update( new Contexts(), new LabelExpression() );
+        }
+        catch( Exception ex )
+        {
+            throw new RuntimeException( ex );
+        }
+    }
+}
