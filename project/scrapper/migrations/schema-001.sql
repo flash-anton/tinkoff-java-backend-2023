dab7ff68655d8a4fbce904b95059e4020471689d
Simonov Anton <flash_anton@mail.ru>
2023-04-11 21:43:37 +0300

    5.1: Добавлена схема БД для отслеживаемых ссылок

---
+++ project/scrapper/migrations/schema-001.sql
@@ -0,0 +1,16 @@
+create table if not exists chat
+(
+    id bigint primary key
+);
+
+create table if not exists link
+(
+    url     text primary key,
+    updated timestamptz default '1970-01-01T00:00:00Z'
+);
+
+create table if not exists chat_link
+(
+    chat_id  bigint references chat on delete cascade,
+    link_url text references link on delete cascade
+);
